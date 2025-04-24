1536c81a8e67d67459a7b660bdb57c7d68286442
Gustavo Souza <afiliadodetrafego@gmail.com>
2025-04-24 19:33:43 -0300

    fix: adiciona host e port para rodar no Render

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ def chat():
     return render_template("chat.html", question=None, answer=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
